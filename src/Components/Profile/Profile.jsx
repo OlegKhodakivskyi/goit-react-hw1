1856dfa86c9df6bd8322b5db356c252e5875f9e2
OlegKhodakivskyi <oleghodakovsky@gmail.com>
2020-08-31 03:17:32 +0300

    rebuild

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,36 +1,33 @@
-import React from "react";
-import styles from "./Profile.module.css";
+import React from 'react';
+import styles from './Profile.module.css';
 
-const Profile = ({name, tag, location, avatar, stats}) => {
+const Profile = ({ name, tag, location, avatar, stats }) => {
   return (
     <div className={styles.profile}>
-  <div className={styles.description}>
-    <img
-      src={avatar}
-      alt="user avatar"
-          className={styles.avatar}
-    />
+      <div className={styles.description}>
+        <img src={avatar} alt="user avatar" className={styles.avatar} />
         <p className={styles.name}>{name}</p>
         <p className={styles.tag}>{tag}</p>
         <p className={styles.location}>{location}</p>
-  </div>
+      </div>
 
       <ul className={styles.stats}>
-    <li>
+        c
+        <li>
           <span className={styles.label}>Followers</span>
           <span className={styles.quantity}>{stats.followers}</span>
-    </li>
-    <li>
+        </li>
+        <li>
           <span className={styles.label}>Views</span>
           <span className={styles.quantity}>{stats.views}</span>
-    </li>
-    <li>
+        </li>
+        <li>
           <span className={styles.label}>Likes</span>
           <span className={styles.quantity}>{stats.likes}</span>
-    </li>
-  </ul>
-      </div>
-  )
-}
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Profile;
